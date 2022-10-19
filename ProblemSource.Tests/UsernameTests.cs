b4f3340acf81b43c85d7430eb660083f5393f6ae
JWMB <jonas.beckeman@gmail.com>
2022-10-19 21:51:14 +0200

    port some username / MnemoJapanese stuff

---
+++ ProblemSource.Tests/UsernameTests.cs
@@ -0,0 +1,64 @@
+﻿using Shouldly;
+
+namespace ProblemSource.Tests
+{
+    public class UsernameTests
+    {
+        [Fact]
+        public void MnemoJapanese_GenerateList()
+        {
+            var dbg = new Dictionary<string, int>();
+            var mnemoJapanese = new MnemoJapanese(2);
+            //var numExtra = 2;
+            for (int i = 0; i < 100; i++)
+            {
+                var val = 280000 + i;
+                //TODO: ? depending on last digits, scramble number for more uniqueness between sequential items
+
+                var str = mnemoJapanese.FromIntWithRandom(val);
+                var decoded = mnemoJapanese.ToIntWithRandom(str);
+                if (decoded == null)
+                    throw new Exception($"'{str}' couldn't be decoded (from {val})");
+                //if (int.Parse(decoded.Value.ToString().Substring(numExtra)) != val)
+                if (decoded.Value != val)
+                    throw new Exception($"'{str}' was decoded to {decoded} instead of {val}");
+
+                dbg.Add(str, val);
+            }
+        }
+
+        [Fact]
+        public void Hash_Dehash()
+        {
+            var mnemoJapanese = new MnemoJapanese(2);
+            var hashedUsername = new HashedUsername(mnemoJapanese, 2);
+
+            var id = 1000;
+            var username = mnemoJapanese.FromIntWithRandom(id);
+            var hashed = hashedUsername.Hash(username);
+
+            var dehashed = hashedUsername.Dehash(hashed);
+            if (dehashed == null)
+                throw new Exception($"Could not dehash {hashed} (value {id})");
+
+            var decodedId = mnemoJapanese.ToIntWithRandom(dehashed);
+
+            decodedId.ShouldBe(id);
+        }
+
+        [Fact]
+        public void Dehash_IncorrectChecksum_ReturnsNull()
+        {
+            var mnemoJapanese = new MnemoJapanese(2);
+            var hashedUsername = new HashedUsername(mnemoJapanese, 2);
+
+            var id = 1000;
+            var username = mnemoJapanese.FromIntWithRandom(id);
+            var hashed = hashedUsername.Hash(username);
+            hashed = $"{(hashed[0] == 'b' ? 'a' : 'b')}{hashed.Substring(1)}";
+            var dehashed = hashedUsername.Dehash(hashed);
+
+            dehashed.ShouldBeNull();
+        }
+    }
+}
--- ProblemSource/MnemoJapanese.cs
+++ ProblemSource/MnemoJapanese.cs
@@ -2,9 +2,10 @@
 {
     public class MnemoJapanese
     {
-        private readonly static Random _rnd = new Random();
-        private static List<string>? symbols = null;
+        private readonly static Random rnd = new Random();
+        private readonly int numRandomDigits;
 
+        private static List<string>? symbols = null;
         public static List<string> Symbols
         {
             get
@@ -20,19 +21,24 @@
             }
         }
 
-        public static string FromIntWithRandom(int value, int numRandomDigits)
+        public MnemoJapanese(int numRandomDigits = 0)
+        {
+            this.numRandomDigits = numRandomDigits;
+        }
+
+        public string FromIntWithRandom(int value)
         {
             var lower = (int)Math.Pow(10, numRandomDigits - 1);
             var upper = (int)Math.Pow(10, numRandomDigits);
-            return FromInt(int.Parse("" + (_rnd.Next(upper - lower) + lower) + "" + value));
+            return FromInt(int.Parse("" + (rnd.Next(upper - lower) + lower) + "" + value));
         }
 
-        public static int? ToIntWithRandom(string value)
+        public int? ToIntWithRandom(string value)
         {
             var decoded = ToInt(value);
             if (decoded == null)
                 return null;
-            return int.Parse(decoded.Value.ToString().Substring(2));
+            return int.Parse(decoded.Value.ToString().Substring(numRandomDigits));
         }
 
         public static string FromInt(int value)
@@ -50,26 +56,104 @@
             var syll = Symbols[index];
             return index + (value.Length > syll.Length ? Symbols.Count * ToInt(value.Substring(syll.Length)) : 0);
         }
+    }
 
-        public static void Test()
+    public class BaseNNumber
+    {
+        private List<int> parts = new();
+        public BaseNNumber(int baseX)
         {
-            var dbg = "";
-            var rnd = new Random();
-            for (int i = 0; i < 100; i++)
-            {
-                var val = 280000 + i;
-                //depending on last digits, scramble number for more uniqueness between sequential items
-                //var c = int.Parse("" + val.ToString()[0]);
+            BaseX = baseX;
+        }
 
-                //var addFirst = rnd.Next(90) + 10; //Always two extra digits first
-                //var useVal = int.Parse(addFirst.ToString() + val);
-                var str = FromIntWithRandom(val, 2);
-                var decoded = ToIntWithRandom(str);
-                dbg += str + " " + val; //+ " " + useVal;
-                if (int.Parse(decoded.ToString().Substring(2)) != val)
-                    dbg += " err: " + decoded;
-                dbg += "\r\n";
+        public int BaseX { get; }
+        public IReadOnlyList<int> Parts => parts;
+
+        public static BaseNNumber CreateFromNumber(int baseX, int value)
+        {
+            var result = new BaseNNumber(baseX);
+            var tmp = new List<int>();
+            while (value > 0) {
+                var val = value % baseX;
+                tmp.Add(val);
+                value = (int)Math.Floor((decimal)value / baseX);
             }
+            tmp.Reverse();
+            result.parts = tmp;
+            return result;
+        }
+
+        public int ToNumber()
+        {
+            var fact = 1;
+            var result = 0;
+            for (var i = parts.Count - 1; i >= 0; i--)
+            {
+                result += parts[i] * fact;
+                fact *= BaseX;
+            }
+            return result;
+        }
+
+        public BaseNNumber ToOtherBase(int baseX) => CreateFromNumber(baseX, ToNumber());
+    }
+
+    public class HashedUsername
+    {
+        // TODO: not well composed, uses static MnemoJapanese methods
+        private readonly int numHashCharacters;
+        private readonly MnemoJapanese mnemoJapanese;
+
+        public HashedUsername(MnemoJapanese mnemoJapanese, int numHashCharacters = 2)
+        {
+            this.mnemoJapanese = mnemoJapanese;
+            this.numHashCharacters = numHashCharacters;
+        }
+
+        public string Hash(string username)
+        {
+            var id = mnemoJapanese.ToIntWithRandom(username);
+            if (id == null) // Was this for backward compatibility..?
+                return username;
+
+            var calculatedHash = CalculateHash(id.Value);
+
+            var splitIndex = 2;
+            return $"{MnemoJapanese.FromInt(calculatedHash)}{username.Substring(0, splitIndex)} {username.Substring(splitIndex)}";
+        }
+
+        public static bool IsHashed(string username) => username.Contains(" ");
+
+        private int CalculateHash(int value)
+        {
+            var bn = BaseNNumber.CreateFromNumber(MnemoJapanese.Symbols.Count, value);
+            var sum = bn.Parts.Sum();
+            return sum % bn.BaseX;
+        }
+
+        public string? Dehash(string hashed_username)
+        {
+            if (!IsHashed(hashed_username))
+                return hashed_username;
+
+            hashed_username = hashed_username.Replace(" ", "");
+
+            var hashString = hashed_username.Substring(0, numHashCharacters);
+            var hashValue = MnemoJapanese.ToInt(hashString);
+            if (hashValue == null)
+                return null;
+
+            var username = hashed_username.Substring(numHashCharacters);
+            var id = mnemoJapanese.ToIntWithRandom(username);
+            if (id == null)
+                return null;
+
+            var calculatedHash = CalculateHash(id.Value);
+
+            if (hashValue != calculatedHash)
+                return null;
+
+            return username;
         }
     }
 }
