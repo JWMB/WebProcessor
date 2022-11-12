8cec67f72d10373af5e616ebbf6f80acf17e833c
JWMB <jonas.beckeman@gmail.com>
2022-11-12 10:06:11 +0100

    throw on azure key forbidden chars

--- ProblemSource/ProblemSourceModule/Services/Storage/AzureTables/TableEntityRepository.cs
+++ ProblemSource/ProblemSourceModule/Services/Storage/AzureTables/TableEntityRepository.cs
@@ -1,6 +1,8 @@
-﻿using Azure.Data.Tables;
+﻿using Azure;
+using Azure.Data.Tables;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ProblemSource.Services.Storage.AzureTables
 {
@@ -9,7 +11,7 @@ namespace ProblemSource.Services.Storage.AzureTables
         private readonly TableClient tableClient;
         private readonly Func<TTableEntity, T> toBusinessObject;
         private readonly Func<T, TTableEntity> toTableEntity;
-        private readonly string partitionKeyForFilter = "0"; // userId
+        private readonly string partitionKeyForFilter; // userId
 
         public TableEntityRepository(TableClient tableClient, Func<TTableEntity, T> toBusinessObject, Func<T, TTableEntity> toTableEntity, string partitionKeyForFilter)
         {
@@ -19,6 +21,40 @@ namespace ProblemSource.Services.Storage.AzureTables
             this.partitionKeyForFilter = partitionKeyForFilter;
         }
 
+        private async Task<List<Response>> Upsert(IEnumerable<ITableEntity> entities)
+        {
+            var result = new List<Response>();
+            foreach (var item in entities)
+            {
+                try
+                {
+                    var response = await tableClient.UpsertEntityAsync(item, TableUpdateMode.Replace);
+                    if (response.IsError)
+                        throw new Exception($"{response.ReasonPhrase}");
+                    result.Add(response);
+                }
+                catch (RequestFailedException rfEx)
+                {
+                    throw;
+                }
+            }
+            return result;
+        }
+
+        private async Task<List<Response>> UpsertBatch(IEnumerable<ITableEntity> entities)
+        {
+            var batch = new List<TableTransactionAction>(entities.Select(f => new TableTransactionAction(TableTransactionActionType.UpsertMerge, f)));
+
+            var response = await tableClient.SubmitTransactionAsync(batch);
+            if (response.Value.Any(o => o.IsError))
+                throw new Exception($"SubmitTransaction errors: {string.Join("\n", response.Value.Where(o => o.IsError).Select(o => o.ReasonPhrase))}");
+
+            return response.Value.ToList();
+        }
+
+        // https://learn.microsoft.com/en-us/rest/api/storageservices/Understanding-the-Table-Service-Data-Model#characters-disallowed-in-key-fields
+        public static Regex InvalidKeyRegex = new Regex(@"\/|\\|#|\?");
+
         public async Task<(IEnumerable<T> Added, IEnumerable<T> Updated)> AddOrUpdate(IEnumerable<T> items)
         {
             if (!items.Any())
@@ -26,26 +62,35 @@ namespace ProblemSource.Services.Storage.AzureTables
 
             var tableEntities = items.Select(toTableEntity).ToList();
 
-            var batch = new List<TableTransactionAction>(tableEntities.Select(f => new TableTransactionAction(TableTransactionActionType.UpsertMerge, f)));
+            var invalidEntries = tableEntities.Where(o => InvalidKeyRegex.IsMatch(o.RowKey) || InvalidKeyRegex.IsMatch(o.PartitionKey));
+            if (invalidEntries.Any())
+            {
+                throw new Exception($"Invalid key(s) for {string.Join(",", tableEntities.Select(o => $"{o.PartitionKey}/{o.RowKey}"))}");
+            }
+
             try
             {
-                var response = await tableClient.SubmitTransactionAsync(batch);
-                if (response.Value.Any(o => o.IsError))
-                    throw new Exception($"SubmitTransaction errors: {string.Join("\n", response.Value.Where(o => o.IsError).Select(o => o.ReasonPhrase))}");
+                var responses = await Upsert(tableEntities);
 
-                var itemAndStatus = items.Select((o, i) => new { Item = o, response.Value[i].Status });
+                var itemAndStatus = items.Select((o, i) => new { Item = o, responses[i].Status });
                 return (itemAndStatus.Where(o => o.Status == 201).Select(o => o.Item), itemAndStatus.Where(o => o.Status != 201).Select(o => o.Item));
             }
-            catch (TableTransactionFailedException ttfEx)
+            catch (Exception ex) when (ex is TableTransactionFailedException || ex is RequestFailedException) //(TableTransactionFailedException ttfEx)
             {
                 string lengthsInfo = "N/A";
                 try
                 {
-                    lengthsInfo = string.Join(", ", tableEntities.Select(o => $"{o.PartitionKey}/{o.RowKey}: {JsonConvert.SerializeObject(o).Length}"));
+                    lengthsInfo = string.Join(", ", tableEntities.Select(o => $"{o.PartitionKey}/{o.RowKey}: {JsonConvert.SerializeObject(o).Length}\n{JsonConvert.SerializeObject(o)}"));
                 }
                 catch { }
 
-                throw new Exception($"{GetType().Name} stored:{lengthsInfo}", ttfEx);
+                var code = ex is TableTransactionFailedException ttfEx ? ttfEx.ErrorCode :
+                    (ex is RequestFailedException rfEx ? rfEx.ErrorCode : null);
+                throw new Exception($"{typeof(T).Name} code:{code} stored:{lengthsInfo}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw;
             }
         }
 
