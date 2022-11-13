c78fbb7b97d8c2b5a50ae9162e2c630ed3c407b3
JWMB <jonas.beckeman@gmail.com>
2022-11-13 20:18:22 +0100

    cleaner AzureTableUserStateRepository

--- ProblemSource/ProblemSourceModule/Services/Storage/AzureTables/AzureTableUserStateRepository.cs
+++ ProblemSource/ProblemSourceModule/Services/Storage/AzureTables/AzureTableUserStateRepository.cs
@@ -1,22 +1,29 @@
 ﻿using Azure.Data.Tables;
+using ProblemSource.Services.Storage.AzureTables.TableEntities;
+using ProblemSourceModule.Services.Storage;
+using System;
 
 namespace ProblemSource.Services.Storage.AzureTables
 {
     public class AzureTableUserStateRepository : IUserStateRepository
     {
-        private readonly AzureTableConfig config;
-        private readonly string partitionKey = "p";
+        private readonly ITableClientFactory tableClientFactory;
+        //private readonly ExpandableTableEntityConverter<Training> converter;
+        //private readonly TableEntityRepository<object, TableEntity> repo;
+        private readonly string staticPartitionKey = "none";
 
-        public AzureTableUserStateRepository(AzureTableConfig config)
+        public AzureTableUserStateRepository(ITableClientFactory tableClientFactory)
         {
-            this.config = config;
+            this.tableClientFactory = tableClientFactory;
+
+            //converter = new ExpandableTableEntityConverter<Training>(t => (staticPartitionKey, t.Id.ToString()));
+            //repo = new TableEntityRepository<Training, TableEntity>(tableClientFactory.UserStates, converter.ToPoco, converter.FromPoco, staticPartitionKey);
         }
 
         public async Task<object?> Get(string uuid)
         {
-            var tableClient = config.CreateTableClient(config.TableUserStates);
-            await tableClient.CreateIfNotExistsAsync();
-            var queryResultsFilter = tableClient.QueryAsync<TableEntity>(filter: $"PartitionKey eq '{partitionKey}' and RowKey eq '{uuid}'");
+            var tableClient = tableClientFactory.UserStates;
+            var queryResultsFilter = tableClient.QueryAsync<TableEntity>(filter: $"PartitionKey eq '{staticPartitionKey}' and RowKey eq '{uuid}'");
 
             await foreach (var entity in queryResultsFilter)
             {
@@ -35,93 +42,10 @@ namespace ProblemSource.Services.Storage.AzureTables
 
         public async Task Set(string uuid, object state)
         {
-            var entity = new TableEntity(partitionKey, uuid);
+            var entity = new TableEntity(staticPartitionKey, uuid);
             AzureTableConfig.SetLongString(entity, Newtonsoft.Json.JsonConvert.SerializeObject(state));
 
-            await config.CreateTableClient(config.TableUserStates).UpsertEntityAsync(entity, TableUpdateMode.Replace);
+            await tableClientFactory.UserStates.UpsertEntityAsync(entity, TableUpdateMode.Replace);
         }
     }
-
-    //class TableEntityBase : ITableEntity
-    //{
-    //    public string PartitionKey { get; set; }
-    //    public string RowKey { get; set; }
-    //    public DateTimeOffset? Timestamp { get; set; }
-    //    public ETag ETag { get; set; }
-
-    //    public TableEntityBase()
-    //    {
-    //    }
-    //    public TableEntityBase(string partitionKey, string rowKey)
-    //    {
-    //        PartitionKey = partitionKey;
-    //        RowKey = rowKey;
-    //    }
-    //}
-
-    //internal class AzStorageEntityAdapter<T> : ITableEntity where T : TableEntityBase, new()
-    //{
-    //    /// <summary>
-    //    /// Gets or sets the entity's partition key
-    //    /// </summary>
-    //    public string PartitionKey
-    //    {
-    //        get => InnerObject.PartitionKey;
-    //        set => InnerObject.PartitionKey = value;
-    //    }
-
-    //    /// <summary>
-    //    /// Gets or sets the entity's row key.
-    //    /// </summary>
-    //    public string RowKey
-    //    {
-    //        get => InnerObject.RowKey;
-    //        set => InnerObject.RowKey = value;
-    //    }
-
-    //    /// <summary>
-    //    /// Gets or sets the entity's Timestamp.
-    //    /// </summary>
-    //    public DateTimeOffset? Timestamp
-    //    {
-    //        get => InnerObject.Timestamp;
-    //        set => InnerObject.Timestamp = value;
-    //    }
-
-    //    /// <summary>
-    //    /// Gets or sets the entity's current ETag.
-    //    /// Set this value to '*' in order to blindly overwrite an entity as part of an update operation.
-    //    /// </summary>
-    //    public ETag ETag
-    //    {
-    //        get => InnerObject.ETag;
-    //        set => InnerObject.ETag = value;
-    //    }
-
-    //    /// <summary>
-    //    /// Place holder for the original entity
-    //    /// </summary>
-    //    public T InnerObject { get; set; }
-
-    //    public AzStorageEntityAdapter()
-    //    {
-    //        // If you would like to work with objects that do not have a default Ctor you can use (T)Activator.CreateInstance(typeof(T));
-    //        InnerObject = new T();
-    //    }
-
-    //    public AzStorageEntityAdapter(T innerObject)
-    //    {
-    //        InnerObject = innerObject;
-    //    }
-
-    //    public virtual void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
-    //    {
-    //        TableEntity.ReadUserObject(this.InnerObject, properties, operationContext);
-    //    }
-
-    //    public virtual IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
-    //    {
-    //        return TableEntity.WriteUserObject(this.InnerObject, operationContext);
-    //    }
-    //}
 }
