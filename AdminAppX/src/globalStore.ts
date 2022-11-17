bf38f1739f0c713e1bd14f48fe3cfde1e86681e0
JWMB <jonas.beckeman@gmail.com>
2022-11-17 06:54:40 +0100

    remove old adminappx

--- AdminAppX/src/globalStore.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import type { ApiFacade } from './apiFacade';
-
-//export const apiFacade = writable(ApiFacade);
-export const apiFacade = writable<ApiFacade>();
--- AdminAppX/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Hello and welcome to my site!</h1>
-<a href="/training">About my site</a>
