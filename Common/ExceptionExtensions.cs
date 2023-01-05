00a4a486ae07c3586a093f1ff04d65586c8fe16a
JWMB <jonas.beckeman@gmail.com>
2023-01-05 17:53:49 +0100

    Extensions

---
+++ Common/ExceptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Common
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsOrContains<T>(this Exception ex, out T? found) where T : Exception
+        {
+            found = null;
+            if (ex is T f)
+                found = f;
+
+            if (ex is AggregateException aEx)
+                found = aEx.InnerExceptions.OfType<T>().FirstOrDefault();
+
+            return found != null;
+        }
+    }
+}
---
+++ Common/IConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Common
+{
+    public static class IConfigurationExtensions
+    {
+        public static T GetOrThrow<T>(this IConfiguration conf, string key)
+        {
+            var val = conf[key];
+            if (val == null)
+                throw new NullReferenceException(key);
+            if (val is T s)
+                return s;
+            var typed = System.Text.Json.JsonSerializer.Deserialize<T>(val);
+            if (typed == null)
+                throw new NullReferenceException($"Couldn't convert {key} to {typeof(T).Name}");
+            return typed;
+        }
+    }
+}
