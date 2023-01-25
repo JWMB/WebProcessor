db0b445f33f3d5233ff0fdd7c83ecbb425e3f019
JWMB <jonas.beckeman@gmail.com>
2023-01-25 11:39:34 +0100

    TrainingSummary: div 0 guards

--- ProblemSource/ProblemSourceModule/Models/Aggregates/TrainingSummary.cs
+++ ProblemSource/ProblemSourceModule/Models/Aggregates/TrainingSummary.cs
@@ -18,6 +18,7 @@ namespace ProblemSourceModule.Models.Aggregates
             {
                 if (TrainedDays == 0) return 0;
                 var diff = Math.Max(1, (int)(LastLogin - FirstLogin).TotalDays);
+                if (diff == 0) return 0;
                 return (decimal)TrainedDays / diff * 7;
             }
         }
@@ -31,7 +32,7 @@ namespace ProblemSourceModule.Models.Aggregates
                 TrainedDays = trainingDays.Count(),
                 AvgResponseMinutes = trainingDays.Average(o => 1M * o.ResponseMinutes),
                 AvgRemainingMinutes = trainingDays.Average(o => 1M * o.RemainingMinutes),
-                AvgAccuracy = trainingDays.Average(o => 1M * o.NumRacesWon / o.NumRaces),
+                AvgAccuracy = trainingDays.Average(o => o.NumRaces == 0 ? 0 : 1M * o.NumRacesWon / o.NumRaces),
                 FirstLogin = trainingDays.Min(o => o.StartTime),
                 LastLogin = trainingDays.Max(o => o.StartTime)
             };
