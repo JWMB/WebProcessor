2d6fe50faed8ea6628713c89715be7f9210ede43
JWMB <jonas.beckeman@gmail.com>
2023-01-27 15:14:14 +0100

    merge fixes

--- ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
+++ ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
@@ -1,42 +1,34 @@
 <script lang="ts">
-	import { apiFacade as apiFacadeStore, loggedInUser } from '../../globalStore';
+	import { apiFacade as apiFacadeStore, loggedInUser } from '../../../globalStore';
 	import { get } from 'svelte/store';
 	import { onMount } from 'svelte';
-<<<<<<<< HEAD:ProblemSource/AdminApp/src/routes/teacher/+page.svelte
-	import type { TrainingSummaryWithDaysDto, TrainingSummaryDto } from 'src/apiClient';
-========
 	import type { TrainingSummaryWithDaysDto, TrainingSummaryDto, TrainingCreateDto, TrainingTemplateDto } from 'src/apiClient';
-	import TrainingsTable from '../../components/trainingsTable.svelte';
-	import TrainingGroupsTable from '../../components/trainingGroupsTable.svelte';
->>>>>>>> main:ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
+	import TrainingsTable from '../../../components/trainingsTable.svelte';
+	import TrainingGroupsTable from '../../../components/trainingGroupsTable.svelte';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
-	import Tabs from 'src/components/tabs.svelte';
-	import ProgressBar from './progress-bar.svelte';
-	import CreateTrainingsModal from './create-trainings-modal.svelte';
-	import { openModal } from 'svelte-modals';
+
+	// let trainingSummaries: TrainingSummary[] = [];
 
 	const apiFacade = get(apiFacadeStore);
+
 	const loggedInUserInfo = get(loggedInUser);
 
-<<<<<<<< HEAD:ProblemSource/AdminApp/src/routes/teacher/+page.svelte
-	let detailedTrainingsData: TrainingSummaryWithDaysDto[] = [];
-	let noOfDays = 7;
-========
 	let templates: TrainingTemplateDto[] = [];
 	let trainingsPromise: Promise<TrainingSummaryWithDaysDto[]>;
 	// let trainingGroupsPromise: Promise<{[key: string]: TrainingSummaryDto[] }>;
 	let trainingGroupsPromise2: Promise<{ group: string; summaries: TrainingSummaryDto[] }[]>;
->>>>>>>> main:ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
 
-	$: trainings = calculateTrainingStats(detailedTrainingsData, noOfDays);
-	let groups: { group: string; summaries: TrainingSummaryDto[] }[];
+	let trainingGroups: { group: string; summaries: TrainingSummaryDto[] }[] = [];
+	let createdTrainingUsernames: string[] = [];
 
-	onMount(() => getData());
+	const clickedGroupRow = (e: CustomEvent<any>) => {
+		trainingsPromise = apiFacade.trainings.getSummaries(e.detail.group);
+	};
+	const clickedTrainingRow = (e: CustomEvent<any>) => {
+		goto(`${base}/training?id=${e.detail.id}`);
+	};
 
-<<<<<<<< HEAD:ProblemSource/AdminApp/src/routes/teacher/+page.svelte
-	async function getData() {
-========
 	async function createTrainings(num: number, groupName: string, numMinutes: number, templateId: number, forUser?: string | null) {
 		if (!groupName) {
 			alert('A name is required');
@@ -62,52 +54,24 @@
 	}
 
 	async function getTrainings() {
->>>>>>>> main:ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
 		if (apiFacade == null) {
 			console.error('apiFacade null');
 			return;
 		}
-		const groupsData = await apiFacade.trainings.getGroups();
-		groups = Object.entries(groupsData).map((o) => ({ group: o[0], summaries: o[1] }));
-	}
+		//trainingsPromise = apiFacade.trainings.getSummaries();
 
-<<<<<<<< HEAD:ProblemSource/AdminApp/src/routes/teacher/+page.svelte
-	async function onSelectGroup(groupId: string) {
-		console.log('onSelectGroup', groupId);
-		detailedTrainingsData = await apiFacade.trainings.getSummaries(groupId);
-	}
-
-	function calculateTrainingStats(data: TrainingSummaryWithDaysDto[], numberOfDays = 7) {
-		return data.map((t) => {
-			return {
-				id: t.id,
-				username: t.username,
-				trainedDays: t.days.length,
-				trainedDaysMax: 30,
-				accuracy: 10,
-				effectiveTime: 22,
-				totalTime: 32,
-				comments: [] as Array<{ type: 'Critical' | 'Warning' | 'Info'; description: string }>
-			};
+		trainingGroupsPromise2 = new Promise((res) => {
+			apiFacade.trainings.getGroups().then((r) => {
+				const asList = Object.entries(r).map((o) => ({ group: o[0], summaries: o[1] }));
+				trainingGroups = asList;
+				res(asList);
+			});
 		});
+		// trainingGroupsPromise = apiFacade.trainings.getGroups();
+		//trainingSummaries = await apiFacade.trainings.getSummaries();
+		//console.log("OK", trainingSummaries.length); // why is TrainingsTable not always updated?
 	}
 
-	function onSelectTraining(trainingId: number) {
-		goto(`${base}/training?id=${trainingId}`);
-	}
-
-	function onCreateGroup() {
-		openModal(CreateTrainingsModal, {
-			onCreateGroup: (id) => {
-				getData();
-			}
-		});
-	}
-</script>
-
-<div class="teacher-view">
-	<h2>Your groups/classes</h2>
-========
 	async function init() {
 		await getTrainings();
 		templates = await apiFacade.trainings.getTemplates();
@@ -139,59 +103,34 @@
 				parseFloat(getElementValue('timePerDay')),
 				parseFloat(getElementValue('template')),
 				getElementValue('forUser'))} />
->>>>>>>> main:ProblemSource/AdminApp/src/routes/admin/teacher/+page.svelte
 
-	{#if groups}
-		<Tabs
-			urlParam="group"
-			tabs={groups.map((g) => {
-				return { id: g.group };
-			})}
-			on:selected={(e) => onSelectGroup(e.detail)}
-			><button on:click={onCreateGroup}>Create new group</button>
-		</Tabs>
-	{/if}
-
-	{#if trainings}
-		<h2>Trainings</h2>
-		<table>
-			<tr>
-				<th>User</th>
-				<th>Days trained</th>
-				<th>Effective time/day</th><th>Accuracy</th><th>Notes</th>
-			</tr>
-			{#each trainings as t (t.id)}
-				<tr on:click={() => onSelectTraining(t.id)}>
-					<td>{t.username}</td>
-					<td><ProgressBar value={t.trainedDays} max={t.trainedDaysMax} suffix="" decimals={0} color="#49e280" /></td>
-					<td><ProgressBar value={t.accuracy} max={100} suffix="%" decimals={0} color="#49e280" /></td>
-					<td>
-						{#each t.comments as c}
-							{c.description}
-						{/each}</td>
-				</tr>
+		{#if createdTrainingUsernames}
+			Created users:
+			{#each createdTrainingUsernames as username}
+				<li>{username}</li>
 			{/each}
-		</table>
-	{/if}
-</div>
+		{/if}
+	</div>
 
-<style>
-	.teacher-view {
-		padding: 20px;
-	}
-	table {
-		height: 100%;
-		text-align: left;
-		width: 100%;
-		border-spacing: 0;
-		border-collapse: collapse;
-	}
-	tr {
-		height: 32px;
-	}
-	th,
-	td {
-		margin-right: 10px;
-		padding: 6px 10px 6px 0;
-	}
-</style>
+	{#await trainingGroupsPromise2}
+		<div>Loading...</div>
+	{:then trainings}
+		{#if !!trainings && trainings.length > 0}
+			<TrainingGroupsTable trainingSummaries={trainings} on:clickedRow={clickedGroupRow} />
+		{/if}
+	{:catch error}
+		{error}
+	{/await}
+
+	<!-- <TrainingsTable trainingSummaries={trainingSummaries} numDays={5}></TrainingsTable> -->
+	{#await trainingsPromise}
+		<div>Loading...</div>
+	{:then trainings}
+		{#if !!trainings && trainings.length > 0}
+			<h2>Trainings</h2>
+			<TrainingsTable trainingSummaries={trainings} numDays={14} on:clickedRow={clickedTrainingRow} />
+		{/if}
+	{:catch error}
+		{error}
+	{/await}
+</div>
---
+++ ProblemSource/AdminApp/src/routes/training/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import TrainingView from '../../components/trainingView.svelte';
+	import { onMount } from 'svelte';
+
+	let accountId: number;
+	onMount(() => {
+		accountId = parseFloat(new URLSearchParams(window.location.search).get('id') ?? '715955');
+	});
+</script>
+
+<TrainingView {accountId} />
