adab9ee22c711a4e197677ae9cabcc69304350c3
Daevid66 <david.sjolander@gmail.com>
2023-01-27 17:30:13 +0100

    switch for stats on 7 last days calculate stats for trainings colors for progress bars

--- ProblemSource/AdminApp/src/routes/teacher/+page.svelte
+++ ProblemSource/AdminApp/src/routes/teacher/+page.svelte
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 	import { apiFacade as apiFacadeStore, loggedInUser } from '../../globalStore';
 	import { get } from 'svelte/store';
@@ -10,12 +9,14 @@
 	import ProgressBar from './progress-bar.svelte';
 	import CreateTrainingsModal from './create-trainings-modal.svelte';
 	import { openModal } from 'svelte-modals';
+	import Switch from 'src/components/switch.svelte';
 
 	const apiFacade = get(apiFacadeStore);
 	const loggedInUserInfo = get(loggedInUser);
 
 	let detailedTrainingsData: TrainingSummaryWithDaysDto[] = [];
-	let noOfDays = 7;
+	let showStatsForLast7days = false;
+	$: noOfDays = showStatsForLast7days ? 7 : 9999;
 
 	$: trainings = calculateTrainingStats(detailedTrainingsData, noOfDays);
 	let groups: { group: string; summaries: TrainingSummaryDto[] }[];
@@ -37,15 +38,23 @@
 	}
 
 	function calculateTrainingStats(data: TrainingSummaryWithDaysDto[], numberOfDays = 7) {
+		const average = (arr: number[]) => {
+			return arr.reduce((p, c) => p + c, 0) / arr.length;
+		};
 		return data.map((t) => {
+			const dateRange = t.days.slice(-numberOfDays);
+			const accuracy = average(dateRange.map((d) => d.numCorrectAnswers / (d.numQuestions || 1))) || 0;
+			const effectiveTime = average(dateRange.map((d) => d.responseMinutes / (d.responseMinutes + d.remainingMinutes || 1))) || 0;
 			return {
 				id: t.id,
 				username: t.username,
 				trainedDays: t.days.length,
 				trainedDaysMax: 30,
-				accuracy: 10,
-				effectiveTime: 22,
-				totalTime: 32,
+				accuracy,
+				effectiveTime,
+				isAccuracyLow: accuracy < 0.4,
+				isEffectiveTimeLow: effectiveTime < 0.5,
+				isDaysTrainedLow: false,
 				comments: [] as Array<{ type: 'Critical' | 'Warning' | 'Info'; description: string }>
 			};
 		});
@@ -62,12 +71,17 @@
 			}
 		});
 	}
+
+	function log(t: any) {
+		console.log('test');
+		return 1;
+	}
 </script>
 
 <div class="teacher-view">
 	<h2>Your groups/classes</h2>
 
-	{#if groups}
+	{#if groups && groups.length > 0}
 		<Tabs
 			urlParam="group"
 			tabs={groups.map((g) => {
@@ -77,20 +91,31 @@
 			><button on:click={onCreateGroup}>Create new group</button>
 		</Tabs>
 	{/if}
+	{#if trainings && trainings.length > 0}
+		<div class="training-header">
+			<h2>Trainings</h2>
+			<label>
+				Show stats for:
+				<span class:activeLabel={!showStatsForLast7days}>All days</span>
+				<Switch bind:checked={showStatsForLast7days} />
+				<span class:activeLabel={showStatsForLast7days}>Last 7 days</span>
+			</label>
+		</div>
 
-	{#if trainings}
-		<h2>Trainings</h2>
 		<table>
 			<tr>
-				<th>User</th>
-				<th>Days trained</th>
-				<th>Effective time/day</th><th>Accuracy</th><th>Notes</th>
+				<th class="user-column">User</th>
+				<th class="days-trained-column">Days trained</th>
+				<th class="effective-time-column">Effective time/day</th>
+				<th class="accuracy-column">Accuracy</th>
+				<th class="notes-column">Notes</th>
 			</tr>
 			{#each trainings as t (t.id)}
 				<tr on:click={() => onSelectTraining(t.id)}>
 					<td>{t.username}</td>
-					<td><ProgressBar value={t.trainedDays} max={t.trainedDaysMax} suffix="" decimals={0} color="#49e280" /></td>
-					<td><ProgressBar value={t.accuracy} max={100} suffix="%" decimals={0} color="#49e280" /></td>
+					<td><ProgressBar value={t.trainedDays} max={t.trainedDaysMax} suffix="" decimals={0} color={t.isDaysTrainedLow ? '#ff5959' : '#c7a0fc'} /></td>
+					<td><ProgressBar value={t.effectiveTime * 100} max={100} suffix="%" decimals={0} color={t.isEffectiveTimeLow ? '#ff5959' : '#49e280'} /></td>
+					<td><ProgressBar value={t.accuracy * 100} max={100} suffix="%" decimals={0} color={t.isAccuracyLow ? '#ff5959' : '#52cad8'} /></td>
 					<td>
 						{#each t.comments as c}
 							{c.description}
@@ -105,6 +130,19 @@
 	.teacher-view {
 		padding: 20px;
 	}
+	.training-header {
+		display: flex;
+		align-items: center;
+		gap: 20px;
+	}
+	.training-header label {
+		display: flex;
+		align-items: center;
+		gap: 5px;
+	}
+	.activeLabel {
+		font-weight: bold;
+	}
 	table {
 		height: 100%;
 		text-align: left;
@@ -112,8 +150,27 @@
 		border-spacing: 0;
 		border-collapse: collapse;
 	}
+	.user-column {
+		width: 120px;
+	}
+	.days-trained-column {
+		width: 120px;
+	}
+	.effective-time-column {
+		width: 160px;
+	}
+	.accuracy-column {
+		width: 160px;
+	}
+	.notes-column {
+		width: auto;
+	}
+
 	tr {
 		height: 32px;
+	}
+	tr:hover {
+		background-color: #e1f1ff;
 	}
 	th,
 	td {
--- ProblemSource/AdminApp/src/routes/teacher/progress-bar.svelte
+++ ProblemSource/AdminApp/src/routes/teacher/progress-bar.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+	import { onMount, tick } from 'svelte';
+
 	export let value = 0;
 	export let max = 100;
 	export let prefix = '';
 	export let suffix = '%';
 	export let decimals = 1;
 	export let color = '#ff0000';
+	export let initialAnimation = true;
 	export let showValueAs: 'All' | 'Percentage' | 'OnlyValue' | 'None' = 'All';
 
-	function getString() {
+	onMount(async () => {
+		if (initialAnimation) {
+			const realValue = value;
+			value = 0;
+			setTimeout(() => {
+				value = realValue;
+			}, 50);
+		}
+	});
+
+	$: valueString = getString(value, max, prefix, suffix, decimals, showValueAs);
+	function getString(value: number, max: number, prefix: string, suffix: string, decimals: number, showValueAs: string) {
 		switch (showValueAs) {
 			case 'All':
 				return prefix + value.toFixed(decimals) + '/' + max.toFixed(decimals) + suffix;
 			case 'Percentage':
-				return prefix + getPercentage().toFixed(decimals) + suffix;
+				return prefix + getPercentage(value, max).toFixed(decimals) + suffix;
 			case 'OnlyValue':
 				return prefix + value.toFixed(decimals) + suffix;
 			case 'None':
@@ -20,21 +34,22 @@
 		}
 	}
 
-	function getPercentage() {
+	$: percentage = getPercentage(value, max);
+	function getPercentage(value: number, max: number) {
 		return (value / max) * 100 || 0;
 	}
 </script>
 
 <div class="progress-bar">
 	<div class="background" />
-	<div class="progress" style:--progress-color={color} style:width={getPercentage() + '%'} />
-	<div class="value">{getString()}</div>
+	<div class="progress" style:--progress-color={color} style:width={percentage + '%'} />
+	<div class="value">{valueString}</div>
 </div>
 
 <style>
 	.progress-bar {
 		height: 100%;
-		border: 1px solid #dddddd;
+		/* border: 1px solid #dddddd; */
 		border-radius: 10px;
 		overflow: hidden;
 		position: relative;
@@ -53,10 +68,15 @@
 		top: 0;
 		bottom: 0;
 		height: 100%;
+		transition: width 0.3s;
 	}
 	.value {
+		display: flex;
+		align-items: center;
 		position: absolute;
 		top: 0;
 		bottom: 0;
+		left: 8px;
+		color: rgba(0, 0, 0, 0.51);
 	}
 </style>
