a15aa9b533bf30cca5334a2347ba522c4b78bfef
JWMB <jonas.beckeman@gmail.com>
2023-06-01 18:29:22 +0200

    overview: stack left mode etc

---
+++ ProblemSource/AdminApp/src/components/DateInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { DateUtils } from "src/utilities/DateUtils";
+
+    // import { onMount } from 'svelte'
+    // export let format = 'YYYY-MM-DD'
+    export let date = new Date();
+      
+    let internal: string;
+  
+    
+    const input = (x: Date) => (internal = DateUtils.toIsoDate(x))
+    const output = (x: string) => (date = DateUtils.toDate(x));
+  
+    $: input(date)
+    $: output(internal)
+  </script>
+  
+  <input type="date" bind:value={internal} />
+  
