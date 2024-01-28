ac2c6056e3196e66206d758e1e9754d3a1bc5cbd
Jonas Beckeman <jonas.beckeman@gmail.com>
2024-01-28 14:23:14 +0100

    BlazorTestBed

---
+++ BlazorTestBed/Program.cs
@@ -0,0 +1,17 @@
+using BlazorTestBed;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+AddServices(builder.Services);
+
+await builder.Build().RunAsync();
+
+void AddServices(IServiceCollection services)
+{
+}
