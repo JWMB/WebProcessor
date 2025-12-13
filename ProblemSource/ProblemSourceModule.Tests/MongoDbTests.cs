f689714e883f1738cc11d1b4f6b073b092a0764c
JWMB <jonas.beckeman@gmail.com>
2025-12-13 15:33:21 +0100

    semi-working

---
+++ ProblemSource/ProblemSourceModule.Tests/MongoDbTests.cs
@@ -0,0 +1,53 @@
+﻿//using EphemeralMongo;
+using Mongo2Go;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using ProblemSource.Models.Aggregates;
+using ProblemSourceModule.Models;
+using ProblemSourceModule.Services.Storage.MongoDb;
+using Shouldly;
+
+namespace ProblemSourceModule.Tests
+{
+    public class MongoDbTests
+    {
+        [Fact]
+        public async Task X()
+        {
+			using var runner = MongoDbRunner.StartForDebugging(additionalMongodArguments: "--quiet --logpath /dev/null");
+
+			//var options = new MongoRunnerOptions
+			//{
+			//	UseSingleNodeReplicaSet = true,
+			//	//StandardOuputLogger = Console.WriteLine,
+			//	StandardOutputLogger = Console.WriteLine,
+			//	StandardErrorLogger = Console.WriteLine,
+			//};
+
+			//BsonClassMap.RegisterClassMap<MongoDocumentWrapper<Training>>(x =>
+			//{
+			//	x.AutoMap();
+			//	x.GetMemberMap(m => m.Id).SetIgnoreIfDefault(true);
+			//});
+
+			var db = new MongoClient(runner.ConnectionString).GetDatabase("_mydb");
+			var sut = new MongoTrainingRepository(db);
+			var training = new Training { Username = "ABV", Created = DateTimeOffset.UtcNow };
+			var id = await sut.AddGetId(training);
+			var retrieved = await sut.Get(id);
+
+			retrieved?.Username.ShouldBe(training.Username);
+
+			var ugdr = new MongoUserGeneratedDataRepositoryProvider(db, id);
+
+			var phase = new Phase { exercise = "a", phase_type = "TEST", training_day = 1, time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() };
+			var result = await ugdr.Phases.Upsert([phase]);
+			result.Added.Count().ShouldBe(1);
+			result.Updated.Count().ShouldBe(0);
+
+			result = await ugdr.Phases.Upsert([phase]);
+			result.Added.Count().ShouldBe(0);
+			result.Updated.Count().ShouldBe(1);
+		}
+	}
+}
--- ProblemSource/ProblemSourceModule/Services/Storage/MongoDb/MongoTrainingPlanRepository.cs
+++ ProblemSource/ProblemSourceModule/Services/Storage/MongoDb/MongoTrainingPlanRepository.cs
@@ -21,10 +21,45 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
 		}
 
         public static string GetCollectionName<T>() => GetCollectionName(typeof(T));
-		public static string GetCollectionName(Type type) => type.Name;
+		public static string GetCollectionName(Type type)
+        {
+            if (type.GenericTypeArguments.Any())
+            {
+                if (type.GetGenericTypeDefinition() == typeof(MongoDocumentWrapper<>))
+                {
+                    return type.GenericTypeArguments[0].Name;
+				}
+            }
+            return type.Name;
+        }
 	}
 
-    public class DbCollection<TDocument, TId>
+    public class DbWrappedCollection<TDocument, TId>
+    {
+        protected DbCollection<MongoDocumentWrapper<TDocument>, TId> collection;
+		public DbWrappedCollection(IMongoDatabase db, string idField, Func<MongoDocumentWrapper<TDocument>, TId> getId)// : base(db, idField, getId)
+        {
+            collection = new DbCollection<MongoDocumentWrapper<TDocument>, TId>(db, idField, getId);
+		}
+
+        public Task Remove(TDocument item) => collection.Remove(new MongoDocumentWrapper<TDocument>(item));
+        public Task Update(TDocument item) => collection.Update(new MongoDocumentWrapper<TDocument>(item)); // TODO: is mongo ID handled properly?
+		public Task Upsert(TDocument item) => collection.Upsert(new MongoDocumentWrapper<TDocument>(item)); // TODO: is mongo ID handled properly?
+		public async Task<TDocument?> Get(TId id)
+        {
+            var found = await collection.Get(id);
+            if (found != null)
+                return found.Document;
+            return default;
+        }
+        public async Task<List<TDocument>> Get(IEnumerable<TId> ids) => (await collection.Get(ids)).Select(o => o.Document).ToList();
+
+
+		public async Task<IEnumerable<TDocument>> GetAll() => (await collection.GetAll()).Select(o => o.Document).ToList();
+
+	}
+
+	public class DbCollection<TDocument, TId>
     {
         protected IMongoCollection<TDocument> collection;
         private readonly string idField;
@@ -36,11 +71,11 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
             this.idField = idField;
             this.getId = getId;
         }
+
 		public async Task<TId> InsertGetId(TDocument item)
 		{
-            await collection.InsertOneAsync(item);
-            //item.Id
-            return getId(item);
+			await collection.InsertOneAsync(item);
+			return getId(item);
         }
 
 		public FilterDefinition<TDocument> GetIdFilter(TDocument id) => GetIdFilter(getId(id), idField); // Builders<TDocument>.Filter.Eq(idField, id);
@@ -52,12 +87,8 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
 		public static FilterDefinition<TDocument> GetIdFilter(TId id, string idField) => Builders<TDocument>.Filter.Eq(idField, id);
 		public static FilterDefinition<TDocument> GetIdFilter(IEnumerable<TId> ids, string idField) => Builders<TDocument>.Filter.AnyIn(idField, ids);
 
-		public async Task<TDocument?> Get(TId id)
-        {
-            //var filter = Builders<TDocument>.Filter.Eq(idField, id);
-            return await (await collection.FindAsync(GetIdFilter(id))).FirstOrDefaultAsync();
-			//await collection.Find(o => o.Email == id).FirstOrDefaultAsync();
-        }
+		public async Task<TDocument?> Get(TId id) => await (await collection.FindAsync(GetIdFilter(id))).FirstOrDefaultAsync();
+		public async Task<List<TDocument>> Get(IEnumerable<TId> ids) => await (await collection.FindAsync(GetIdFilter(ids))).ToListAsync();
 
 		public async Task<IEnumerable<TDocument>> GetAll() => await collection.Find(o => true).ToListAsync();
 
@@ -67,7 +98,7 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
 		}
 
 		public async Task Update(TDocument item) => await collection.FindOneAndReplaceAsync(GetIdFilter(getId(item)), item);
-        public async Task Upsert(TDocument item) => await Update(item);
+        public async Task Upsert(TDocument item) => await collection.FindOneAndReplaceAsync(GetIdFilter(getId(item)), item, new FindOneAndReplaceOptions<TDocument, TDocument> { IsUpsert = true });
 
 		public async Task<List<TDocument>> ListAsync(FilterDefinition<TDocument> filter, CancellationToken cancellationToken = default)
 		{
@@ -91,18 +122,18 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
 
 			return (list.Except(upserted), upserted);
 		}
-		//protected async Task<IAsyncCursor<TProjection>> FindAsync<TProjection>(FilterDefinition<TDocument> filter, FindOptions<TDocument, TProjection>? options = null, CancellationToken cancellationToken = default(CancellationToken))
-		//{
-		//    if (options == null) throw new ArgumentNullException(nameof(options));
-		//    return await collection.FindAsync<TDocument>(filter, options, cancellationToken);
-		//}
 
+        public async Task<long> CountDocumentsAsync(FilterDefinition<TDocument>? filter = null)
+        {
+			return await collection.CountDocumentsAsync(filter ?? Builders<TDocument>.Filter.Empty, filter == null ? new CountOptions { Hint = "_id_" } : null);
+		}
 	}
 
 	public class MongoUserGeneratedDataRepositoryProvider : IUserGeneratedDataRepositoryProvider
     {
         private readonly IMongoDatabase db;
         private readonly int trainingId;
+        private readonly string Key = $"{nameof(MongoDocumentWrapper<int>)}.{nameof(MongoDocumentWrapper<int>.RowKey)}";
 
         public MongoUserGeneratedDataRepositoryProvider(IMongoDatabase db, int trainingId)
         {
@@ -110,7 +141,7 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
             this.trainingId = trainingId;
         }
 
-        public IBatchRepository<Phase> Phases => new MongoBatchRepository<Phase, string>(db, "", Phase.UniqueIdWithinUser, trainingId);
+        public IBatchRepository<Phase> Phases => new MongoBatchRepository<Phase, string>(db, Key, Phase.UniqueIdWithinUser, trainingId);
 
         public IBatchRepository<TrainingDayAccount> TrainingDays => throw new NotImplementedException();
 
@@ -128,44 +159,62 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
 
     public class MongoDocumentWrapper<TDocument>
     {
+        public MongoDocumentWrapper()
+        {
+			Id = ObjectId.GenerateNewId();
+		}
+
+		[System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+		public MongoDocumentWrapper(TDocument doc) : this()
+        {
+            Document = doc;
+        }
+
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
+		public ObjectId Id { get; set; }
+
+		//[BsonRepresentation(BsonType.ObjectId)]
+		//[BsonId(IdGenerator = typeof(MongoDB.Bson.Serialization.IdGenerators.BsonObjectIdGenerator))]
+		//[BsonIgnoreIfDefault]
+		//public string Id { get; set; } = string.Empty;
+
+		public string RowKey { get; set; } = string.Empty;
 
 		public required TDocument Document { get; set; }
-    }
+	}
 
     public class MongoBatchRepository<TDocument, TId> : IBatchRepository<TDocument>
     {
         private readonly int trainingId;
         private readonly string trainingIdField;
 		//private readonly Func<TDocument, TId> getId;
-		private readonly DbCollection<TDocument, TId> collection;
-		//private readonly DbCollection<MongoDocumentWrapper<TDocument>, TId> collection;
+		//private readonly DbCollection<TDocument, TId> collection;
+		private readonly DbCollection<MongoDocumentWrapper<TDocument>, TId> collection;
 
 		public MongoBatchRepository(IMongoDatabase db, string idField, Func<TDocument, TId> getId, int trainingId, string trainingIdField = "trainingId")
         {
-			collection = new DbCollection<TDocument, TId>(db, idField, getId);
-			//collection = new DbCollection<MongoDocumentWrapper<TDocument>, TId>(db, $"{nameof(MongoDocumentWrapper<TDocument>.Document)}.{idField}", item => getId(item.Document));
+			//collection = new DbCollection<TDocument, TId>(db, idField, getId);
+			collection = new DbCollection<MongoDocumentWrapper<TDocument>, TId>(db, $"{nameof(MongoDocumentWrapper<TDocument>.Document)}.{idField}", item => getId(item.Document));
 
 			this.trainingId = trainingId;
             this.trainingIdField = trainingIdField;
             //this.getId = getId;
         }
 
-		//private FilterDefinition<MongoDocumentWrapper<TDocument>> GetTrainingIdFilter() => DbCollection<MongoDocumentWrapper<TDocument>, int>.GetIdFilter(trainingId, $"{nameof(MongoDocumentWrapper<TDocument>.Document)}.{trainingIdField}");
-		private FilterDefinition<TDocument> GetTrainingIdFilter() => DbCollection<TDocument, int>.GetIdFilter(trainingId, trainingIdField);
-		//public async Task<IEnumerable<TDocument>> GetAll() => (await collection.ListAsync(GetTrainingIdFilter())).Select(o => o.Document).ToList();
-		public async Task<IEnumerable<TDocument>> GetAll() => await collection.ListAsync(GetTrainingIdFilter());
+        private FilterDefinition<MongoDocumentWrapper<TDocument>> GetTrainingIdFilter() => DbCollection<MongoDocumentWrapper<TDocument>, int>.GetIdFilter(trainingId, $"{nameof(MongoDocumentWrapper<TDocument>.Document)}.{trainingIdField}");
+        public async Task<IEnumerable<TDocument>> GetAll() => (await collection.ListAsync(GetTrainingIdFilter())).Select(o => o.Document).ToList();
+        //private FilterDefinition<TDocument> GetTrainingIdFilter() => DbCollection<TDocument, int>.GetIdFilter(trainingId, trainingIdField);
+        //public async Task<IEnumerable<TDocument>> GetAll() => await collection.ListAsync(GetTrainingIdFilter());
 
-		public async Task<int> RemoveAll()
+        public async Task<int> RemoveAll()
             => await collection.RemoveAsync(GetTrainingIdFilter());
 
         public async Task<(IEnumerable<TDocument> Added, IEnumerable<TDocument> Updated)> Upsert(IEnumerable<TDocument> items)
         {
-            Func<TDocument, FilterDefinition<TDocument>> createFilter = item =>
-                Builders<TDocument>.Filter.And(GetTrainingIdFilter(), collection.GetIdFilter(item));
-            return await collection.Upsert(items, createFilter);
+            Func<MongoDocumentWrapper<TDocument>, FilterDefinition<MongoDocumentWrapper<TDocument>>> createFilter = item =>
+                Builders<MongoDocumentWrapper<TDocument>>.Filter.And(GetTrainingIdFilter(), collection.GetIdFilter(item));
+            var (added, upserted) = await collection.Upsert(items.Select(o => new MongoDocumentWrapper<TDocument>(o)), createFilter);
+            return (added.Select(o => o.Document), upserted.Select(o => o.Document));
         }
 	}
 
@@ -178,25 +227,42 @@ namespace ProblemSourceModule.Services.Storage.MongoDb
         //public async Task<User?> Get(string id) => await collection.Find(o => o.Email == id).FirstOrDefaultAsync();
 	}
 
-    public class MongoTrainingRepository : DbCollection<Training, int>, ITrainingRepository
+	public class MongoTrainingRepository : DbWrappedCollection<Training, int>, ITrainingRepository
+
+	//public class MongoTrainingRepository : DbCollection<Training, int>, ITrainingRepository
     {
-		public MongoTrainingRepository(IMongoDatabase db) : base(db, nameof(Training.Id), u => u.Id)
+		private SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+		public MongoTrainingRepository(IMongoDatabase db) : base(db, $"{nameof(MongoDocumentWrapper<int>.Document)}.{nameof(Training.Id)}", u => u.Document.Id)
 		{ }
 
         public Task Add(Training item) => AddGetId(item);
 
-        public async Task<int> AddGetId(Training item) => await InsertGetId(item);
+        public async Task<int> AddGetId(Training item)
+        {
+			await semaphore.WaitAsync();
+
+			var filter = Builders<MongoDocumentWrapper<Training>>.Filter.Empty;
+			var count = await collection.CountDocumentsAsync();
+            item.Id = (int)count + 1;
+            //await collection.Upsert(new MongoDocumentWrapper<Training>(item));
+			await Upsert(item);
+			//await InsertGetId(item);
+
+			semaphore.Release();
+            return item.Id;
+        }
 
         public async Task<IEnumerable<Training>> GetByIds(IEnumerable<int> ids)
         {
             var list = ids.ToList();
-			return await (await collection.FindAsync(o => list.Contains(o.Id))).ToListAsync();
-        }
+            return await Get(ids);
+			//return await (await collection.FindAsync(o => list.Contains(o.Id))).ToListAsync();
+		}
 		//public Task Remove(Training item) => throw new NotImplementedException();
 		//public Task Update(Training item) => throw new NotImplementedException();
 		//public Task Upsert(Training item) => throw new NotImplementedException();
 		//public async Task<Training?> Get(int id) => await base.Get(id);
 		//public Task<IEnumerable<Training>> GetAll() => throw new NotImplementedException();
 	}
-
 }
