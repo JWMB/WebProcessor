94b53a5708a8439ef38be3882bd82939f85e2966
JWMB <jonas.beckeman@gmail.com>
2026-07-09 10:35:37 +0200

    attempts to handle env

--- ProblemSource/AdminApp/src/startup.ts
+++ ProblemSource/AdminApp/src/startup.ts
@@ -26,5 +26,13 @@ export class Startup {
 export function resolveLocalServerBaseUrl(location: Location) {
     console.log("PUBLIC_LOCAL_SERVER_PATH", PUBLIC_LOCAL_SERVER_PATH);
     console.log("dyn", envDyn, "meta", import.meta.env);
-    return envDyn.PUBLIC_LOCAL_SERVER_PATH || PUBLIC_LOCAL_SERVER_PATH || location.origin;
+    let result = envDyn.PUBLIC_LOCAL_SERVER_PATH || PUBLIC_LOCAL_SERVER_PATH || location.origin;
+
+    // hm, seems .env.local is used even in docker/podman..?
+    if (import.meta.env.PUBLIC_LOCAL_SERVER_PATH2)
+        result = import.meta.env.PUBLIC_LOCAL_SERVER_PATH2;
+    if (envDyn.PUBLIC_LOCAL_SERVER_PATH2)
+        result = envDyn.PUBLIC_LOCAL_SERVER_PATH2;
+
+    return result;
 }
